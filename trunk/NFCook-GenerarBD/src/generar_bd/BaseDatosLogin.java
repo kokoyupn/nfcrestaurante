8624786f1f84621c636b43bc06870e2b55337581
dannyfg1990@gmail.com <dannyfg1990@gmail.com@3336fd84-853c-92c1-6b51-ecfb7933bf2d>
2013-03-18 21:31:33 +0000

    Changes on BBDD Login -----------ESPAÑOL-------- Cambios en la base de datos login, nuevo nombre.

--- trunk/NFCook-GenerarBD/src/generar_bd/BaseDatosLogin.java
+++ trunk/NFCook-GenerarBD/src/generar_bd/BaseDatosLogin.java
@@ -30,57 +30,57 @@ public class BaseDatosLogin extends Activity {
      		
         } 
 
-        ContentValues n21 = new ContentValues();
-     	n21.put("Nombre", "Abel");
-     	n21.put("Contraseña", "1234");
-     	db.insert("Camareros", null, n21);
+        ContentValues usuario = new ContentValues();
+        usuario.put("Nombre", "Abel");
+        usuario.put("Contraseña", "1234");
+     	db.insert("Camareros", null, usuario);
      	           
     	
-     	ContentValues n22 = new ContentValues();
-     	n22.put("Nombre", "Carlos");
-     	n22.put("Contraseña", "1234");
-     	db.insert("Camareros", null, n22);
+     	usuario= new ContentValues();
+     	usuario.put("Nombre", "Carlos");
+     	usuario.put("Contraseña", "1234");
+     	db.insert("Camareros", null, usuario);
      	
-     	ContentValues n23 = new ContentValues();
-     	n23.put("Nombre", "Javi");
-     	n23.put("Contraseña", "1234");
-     	db.insert("Camareros", null, n23);
+     	usuario = new ContentValues();
+     	usuario.put("Nombre", "Javi");
+     	usuario.put("Contraseña", "1234");
+     	db.insert("Camareros", null, usuario);
      	
-     	ContentValues n24 = new ContentValues();
-     	n24.put("Nombre", "Dani");
-     	n24.put("Contraseña", "1234");
-     	db.insert("Camareros", null, n24);
+     	usuario = new ContentValues();
+     	usuario.put("Nombre", "Dani");
+     	usuario.put("Contraseña", "1234");
+     	db.insert("Camareros", null, usuario);
      	
-     	ContentValues n25 = new ContentValues();
-     	n25.put("Nombre", "Rober");
-     	n25.put("Contraseña", "1234");
-     	db.insert("Camareros", null, n25);
+     	usuario = new ContentValues();
+     	usuario.put("Nombre", "Rober");
+     	usuario.put("Contraseña", "1234");
+     	db.insert("Camareros", null, usuario);
      	
-     	ContentValues n26 = new ContentValues();
-     	n26.put("Nombre", "AlexM");
-     	n26.put("Contraseña", "1234");
-     	db.insert("Camareros", null, n26);
+     	usuario = new ContentValues();
+     	usuario.put("Nombre", "AlexM");
+     	usuario.put("Contraseña", "1234");
+     	db.insert("Camareros", null, usuario);
      	
-     	ContentValues n27 = new ContentValues();
-     	n27.put("Nombre", "AlexV");
-     	n27.put("Contraseña", "1234");
-     	db.insert("Camareros", null, n27);
+     	usuario = new ContentValues();
+     	usuario.put("Nombre", "AlexV");
+     	usuario.put("Contraseña", "1234");
+     	db.insert("Camareros", null, usuario);
      	
-     	ContentValues n28 = new ContentValues();
-     	n28.put("Nombre", "JuanDiego");
-     	n28.put("Contraseña", "1234");
-     	db.insert("Camareros", null, n28);
+     	usuario = new ContentValues();
+     	usuario.put("Nombre", "JuanDiego");
+     	usuario.put("Contraseña", "1234");
+     	db.insert("Camareros", null, usuario);
      	
-     	ContentValues n29 = new ContentValues();
-     	n29.put("Nombre", "Alvaro");
-     	n29.put("Contraseña", "1234");
-     	db.insert("Camareros", null, n29);
+     	usuario = new ContentValues();
+     	usuario.put("Nombre", "Alvaro");
+     	usuario.put("Contraseña", "1234");
+     	db.insert("Camareros", null, usuario);
      	
-     	ContentValues n30 = new ContentValues();
-     	n30.put("Nombre", "admin");
-     	n30.put("Contraseña", "admin");
-     	db.insert("Camareros", null, n30);
-/************************************************FIN FOSTER*************************************************/
+     	usuario = new ContentValues();
+     	usuario.put("Nombre", "admin");
+     	usuario.put("Contraseña", "admin");
+     	db.insert("Camareros", null, usuario);
+
         // Cerramos la base de datos
      	sql.close();
     }
