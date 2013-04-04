3f7128cc52d7d71d48021920b9e77f8f213ac36f
alvarorm89@gmail.com <alvarorm89@gmail.com@3336fd84-853c-92c1-6b51-ecfb7933bf2d>
2013-04-04 11:44:14 +0000

    Added QR reading
    Añadida la lectura por sincronización de QR

--- trunk/NFcook-Camarero/src/com/example/nfcook_camarero/Sincronizacion_QR.java
+++ trunk/NFcook-Camarero/src/com/example/nfcook_camarero/Sincronizacion_QR.java
@@ -2,7 +2,9 @@ package com.example.nfcook_camarero;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 
@@ -24,7 +26,26 @@ public class Sincronizacion_QR extends Activity {
 		numMesa = bundle.getString("NumMesa");
 		numPersonas = bundle.getString("Personas");
 		idCamarero = bundle.getString("IdCamarero");
+		
+		//Lectura QR
+		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+			intent.putExtra("SCAN_MODE","QR_CODE_MODE");
+			startActivityForResult(intent,0);
 
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+           if (resultCode == RESULT_OK) {
+              String contents = intent.getStringExtra("SCAN_RESULT");
+              // Handle successful scan
+             Toast toast1 = Toast.makeText(getApplicationContext(), contents, Toast.LENGTH_SHORT);
+         
+                toast1.show();
+           } else if (resultCode == RESULT_CANCELED) {
+              // Handle cancel
+           }
+        }
 	}
 	
 }
