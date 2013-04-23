c7747c6ecda7036b17ae85624bfe0169f9073387
avillapalos <avillapalos@gmail.com>
2013-04-23 17:51:52 +0000

    Fixed small details in user's app for delivery. ----------- ESPAÑOL ------------------------- Arreglados pequeños detalles de la aplicación de usuario para la entrega.

--- NFCook-Usuario/src/fragments/ListaMapasFragment.java
+++ NFCook-Usuario/src/fragments/ListaMapasFragment.java
@@ -4,22 +4,39 @@ import com.example.nfcook.R;
 
 import adapters.ListaMapasAdapter;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import usuario.DescripcionRestaurante;
 import usuario.Mapas;
 import android.app.Fragment;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ListaMapasFragment extends Fragment {
+	private View vista;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View vista = inflater.inflate(R.layout.lista_mapas, container, false);
+		vista = inflater.inflate(R.layout.lista_mapas, container, false);
 		
 		ListView listView = (ListView) vista.findViewById(R.id.listViewMapas);
 		ListaMapasAdapter adapter = new ListaMapasAdapter(getActivity().getApplicationContext(), Mapas.getRestaurantes());
 		listView.setAdapter(adapter);
+		
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> arg0, View arg1, int pos,
+					long arg3) {
+				String rest = Mapas.getRestaurantes().get(pos).getNombre();
+				Intent intent = new Intent(getActivity().getBaseContext(),DescripcionRestaurante.class);
+				intent.putExtra("nombreRestaurante", rest);
+		    	startActivity(intent);
+				
+			}
+		});
 		
 		return vista;
 	}
