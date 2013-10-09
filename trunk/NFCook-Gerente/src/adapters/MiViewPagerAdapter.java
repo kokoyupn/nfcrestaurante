794c61388ef51a0e5894f4eab9d9d466a6ed6cb6
alejandromoran90@gmail.com <alejandromoran90@gmail.com@3336fd84-853c-92c1-6b51-ecfb7933bf2d>
2013-10-09 17:30:39 +0000

    Import of the new Owner App. With the login and information screens.

---
+++ trunk/NFCook-Gerente/src/adapters/MiViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package adapters;
+
+import java.util.ArrayList;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * 
+ * @author Alejandro Moran
+ * 
+ * PagerAdapter es una clase que gestiona los Fragments para
+ * que se mantengan en memoria como cuando se usa en pestañas como en
+ * nuestro caso.
+ * 
+ * Tiene una lista de los Fragments que estamos usando.
+ * 
+ * En las pestañas de la interfaz de camarero se guarda por orden:
+ * 0-> Información
+ * 1-> Estadísticas
+ * 2-> Clasificación de platos
+ * 
+ * Cuando el TabHost o el ViewPager necesite acceder a un Fragment
+ * invocará a getItem.
+ *
+ */
+public class MiViewPagerAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<Fragment> fragments;
+    
+    public MiViewPagerAdapter(FragmentManager fragmentManager, ArrayList<Fragment> fragments) {
+            super(fragmentManager);
+            this.fragments = fragments;
+    }
+
+	@Override
+    public Fragment getItem(int position) {
+            return this.fragments.get(position);
+    }
+    
+    @Override
+    public int getCount() {
+            return this.fragments.size();
+    }
+}
---
+++ trunk/NFCook-Gerente/src/com/example/nfcook_gerente/InfoRestaurante.java
@@ -0,0 +1,14 @@
+package com.example.nfcook_gerente;
+
+import android.app.Activity;
+
+
+/**
+ * @author: Alejandro Moran
+ * 
+ * Esta clase contendrá toda la información de un restaurante
+ * 
+**/
+public class InfoRestaurante extends Activity{
+
+}
