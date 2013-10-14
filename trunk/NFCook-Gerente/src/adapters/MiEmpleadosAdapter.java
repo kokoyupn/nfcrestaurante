074d968bd2513ad8c875562fcb510df8ceac4cd6
rober_571@hotmail.com <rober_571@hotmail.com@3336fd84-853c-92c1-6b51-ecfb7933bf2d>
2013-10-14 20:53:55 +0000

    Fragment de la informacion de los empleados de un restaurante y la pantalla con la informacion de cada empleado; con sus layouts correspondientes. Una primera base de datos de los empleados.

---
+++ trunk/NFCook-Gerente/src/adapters/MiEmpleadosAdapter.java
@@ -0,0 +1,70 @@
+package adapters;
+
+import java.util.ArrayList;
+
+import com.example.nfcook_gerente.R;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+
+
+public class MiEmpleadosAdapter extends BaseAdapter{
+	private ArrayList<PadreListaEmpleados> contenido;
+	private Activity activity;
+	private Context context;
+	 
+	 public MiEmpleadosAdapter(Activity activity, ArrayList<PadreListaEmpleados> contenido) {
+		 this.activity = activity;
+		 this.context = activity.getApplicationContext();
+		 this.contenido = contenido;
+	}
+	 
+	public int getCount() {
+		return contenido.size();
+	}
+	
+	public Object getItem(int pos) {
+		return contenido.get(pos);
+	}
+	
+	public long getItemId(int pos) {
+		return 0;//contenido.get(pos).getDni();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View vista = convertView;
+        
+	    if(convertView == null) {
+	    	LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	        vista = inflater.inflate(R.layout.contenido_lista_empleados, null);
+	    }
+	             
+	    PadreListaEmpleados empleado = contenido.get(position);
+	     
+	    ImageView foto = (ImageView) vista.findViewById(R.id.imagenEmpleado);
+	    foto.setImageResource(context.getResources().getIdentifier(contenido.get(position).getFoto(),"drawable",context.getPackageName()));	
+		
+	    TextView nombre = (TextView) vista.findViewById(R.id.nombre);
+	    nombre.setText(empleado.getNombre()+" "+empleado.getApellido1()+" "+empleado.getApellido2());
+	    
+	    TextView puesto = (TextView) vista.findViewById(R.id.puesto);
+	    puesto.setText("Puesto: " + empleado.getPuesto());
+	    
+	    TextView idEmpleado = (TextView) vista.findViewById(R.id.idEmpleado);
+	    idEmpleado.setText("Numero de empleado: " + empleado.getIdEmpleado());
+	    
+	   return vista;
+	}
+}
+
+	
+
+
---
+++ trunk/NFCook-Gerente/src/com/example/nfcook_gerente/InfoEmpleado.java
@@ -0,0 +1,232 @@
+package com.example.nfcook_gerente;
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+
+import adapters.PadreListaEmpleados;
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import baseDatos.HandlerGenerico;
+
+import com.androidplot.xy.*;
+
+public class InfoEmpleado extends Activity {
+
+	
+	private String idEmpleado;
+	
+	private XYPlot mySimpleXYPlot;
+
+	private HandlerGenerico sqlEmpleados;
+
+	private SQLiteDatabase dbEmpleados;
+	
+	
+	private String fechaNacimiento;
+	private String domicilio;
+	private String dni;
+	private String puesto;
+	private String apellido2;
+	private String nombre;
+	private String foto;
+	private String apellido1;
+	
+	ImageView imageViewFoto;
+	FrameLayout contenedorFoto;
+	TextView nombreYApellidos;
+	Button dia, mes, anio;
+	//Spinner rango;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.informacion_empleados);
+		
+		Bundle bundle = getIntent().getExtras();
+		idEmpleado = bundle.getString("IdEmpleado");
+		/*nombre = "Pepe";
+		apellido1 = "garcia";
+		apellido2="martin";*/
+		
+		importarBaseDatos();
+		
+		
+		cargarInfoEmpleado();
+		
+		mostrarInfo();
+		
+		funcionalidadCalendario();
+		
+		funcionalidadBotones();
+		
+		
+		
+		
+		
+		
+	}
+	
+	private void funcionalidadBotones() {
+		dia = (Button)findViewById(R.id.dia);
+		mes = (Button)findViewById(R.id.mes);
+		anio = (Button)findViewById(R.id.anio);
+		
+		dia.setText("Dia");
+		mes.setText("Mes");
+		anio.setText("Anio");
+		
+		mySimpleXYPlot = (XYPlot) findViewById(R.id.graficaIngresos);
+		
+		dia.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            	mySimpleXYPlot.clear();
+            	
+            	//En el eje X pone tantos valores como le metas al eje Y, en este caso de 9h a 23h
+            	Number[] ejeY = {4,10,11,7,13,3,8,16,36,18,19,23,55,1,9};//Son los valores del eje Y
+            	
+                XYSeries series1 = new SimpleXYSeries(
+                        Arrays.asList(ejeY),  // Array de datos
+                        SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Sólo valores verticales
+                        "Facturacion"); // Nombre de la primera serie
+                
+                //Modificamos los colores de la primera serie color de linea, color de punto, relleno respectivamente
+                LineAndPointFormatter formato = 
+                		new LineAndPointFormatter( Color.rgb(0, 200, 0),Color.rgb(0, 100, 0),Color.rgb(150, 190, 150), null );
+         
+                //Una vez definida la serie (datos y estilo), la añadimos al panel
+                mySimpleXYPlot.addSeries(series1, formato);
+                mySimpleXYPlot.redraw();
+            }});	
+		
+		mes.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            	mySimpleXYPlot.clear();
+            	//Meter 12 valores para que ponga 30 valores en el eje X
+            	Number[] ejeY = {2,10,4,5,20,5,34,100,76,23,122,56,
+            			2,10,4,5,20,5,34,100,76,23,122,56,2,10,4,5,20,5};
+            	
+                XYSeries series1 = new SimpleXYSeries(
+                        Arrays.asList(ejeY),  // Array de datos
+                        SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Sólo valores verticales
+                        "Facturacion"); // Nombre de la primera serie
+                
+                //Modificamos los colores de la primera serie color de linea, color de punto, relleno respectivamente
+                LineAndPointFormatter formato = 
+                		new LineAndPointFormatter( Color.rgb(0, 200, 0),Color.rgb(0, 100, 0),Color.rgb(150, 190, 150), null );
+         
+                //Una vez definida la serie (datos y estilo), la añadimos al panel
+                mySimpleXYPlot.addSeries(series1, formato);
+                mySimpleXYPlot.redraw();
+            }});
+		anio.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            	mySimpleXYPlot.clear();
+            	
+            	Number[] ejeY = {2,10,4,5,20,5,34,100,76,23,122,56};//Son los valores del eje Y
+            	
+            	
+                XYSeries series1 = new SimpleXYSeries(
+                        Arrays.asList(ejeY),  // Array de datos
+                        SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Sólo valores verticales
+                        "Facturacion"); // Nombre de la primera serie
+                
+                //Modificamos los colores de la primera serie color de linea, color de punto, relleno respectivamente
+                LineAndPointFormatter formato = 
+                		new LineAndPointFormatter( Color.rgb(0, 200, 0),Color.rgb(0, 100, 0),Color.rgb(150, 190, 150), null );
+         
+                //Una vez definida la serie (datos y estilo), la añadimos al panel
+                mySimpleXYPlot.addSeries(series1, formato);
+                mySimpleXYPlot.redraw();
+            }});
+		
+	}
+
+	
+
+	private void mostrarInfo() {
+		imageViewFoto = (ImageView)findViewById(R.id.fotoEmpleado);
+		imageViewFoto.setImageResource(getResources().getIdentifier(foto,"drawable",this.getPackageName()));
+		
+		
+		nombreYApellidos = (TextView)findViewById(R.id.datosEmpleado);
+		nombreYApellidos.setText(nombre + '\n' + apellido1 + '\n' + apellido2 + '\n' +
+				puesto + '\n' + dni + '\n' + domicilio + '\n' + fechaNacimiento);
+	    
+		//Display display = getWindowManager().getDefaultDisplay();
+		//int ancho = display.getWidth();
+		//imageViewFoto.getLayoutParams().width = 10;
+		//FrameLayout.LayoutParams parametros = new FrameLayout.LayoutParams(,ancho/4);
+		//imageViewFoto.setLayoutParams(parametros);
+		
+	}
+
+	private void cargarInfoEmpleado() {
+		try{
+			String[] campos = new String[]{"Foto","Nombre","Apellido1","Apellido2","Puesto","IdEmpleado","DNI","Domicilio","FechaNacimiento"};
+		   
+			String[] datos = new String[]{idEmpleado};
+			Cursor c = dbEmpleados.query("Empleados",campos, "idEmpleado=?",datos, null, null,null);
+		    
+		   c.moveToNext();
+		   foto = c.getString(0);
+		   puesto = c.getString(4);
+		   idEmpleado = c.getString(5);
+		   dni = c.getString(6);
+		   domicilio = c.getString(7);
+		   fechaNacimiento = c.getString(8);
+		   
+		  
+		   
+		}catch(Exception e){
+			System.out.println("Error en cargarInfoEmpleado");
+			
+			
+		}
+		
+	}
+
+	private void funcionalidadCalendario() {
+		/*
+		Calendar nextYear = Calendar.getInstance();
+		nextYear.add(Calendar.YEAR, 1);
+
+		calendario = (CalendarPickerView) findViewById(R.id.calendario);
+		Date today = new Date();
+		calendario.init(today, nextYear.getTime()).withSelectedDate(today);*/
+		
+	}
+
+	
+
+	public void importarBaseDatos(){
+		 try{
+				sqlEmpleados = new HandlerGenerico(getApplicationContext(), "/data/data/com.example.nfcook_gerente/databases/", "Empleados.db");
+				dbEmpleados = sqlEmpleados.open();
+			}catch(SQLiteException e){
+				System.out.println("CATCH");
+				Toast.makeText(getApplicationContext(),"NO EXISTE LA BASE DE DATOS",Toast.LENGTH_SHORT).show();
+			}
+	}
+		
+}
