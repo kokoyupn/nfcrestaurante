ab3927ecea8a4522752adafc5ca699c825f77b74
alejandromoran90@gmail.com <alejandromoran90@gmail.com@3336fd84-853c-92c1-6b51-ecfb7933bf2d>
2013-10-18 13:48:21 +0000

    UI changed in Info Restaurant screen. Trying to change the Tabs, testing. New pictures of the restaurants (with border). AndroidManifest modify to allow Location services with Internet and Phone Calling.

--- trunk/NFCook-Gerente/src/com/example/nfcook_gerente/GeneralRestaurantes.java
+++ trunk/NFCook-Gerente/src/com/example/nfcook_gerente/GeneralRestaurantes.java
@@ -20,9 +20,13 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageView;
 import android.widget.ListView;
+ 
+/** 
+=======
 import android.widget.Toast;
  
 /** 
+>>>>>>> .r521
  * Clase que se encarga de cargar el adapter y de la pantalla inicial del gerente
  * También se establecen los onClick de los botones y su comportamiento (Cuando aparecer y desaparecer)
  * Lee de base de datos los restaurantes y los carga en el ArrayList restaurantes
@@ -62,8 +66,9 @@ public class GeneralRestaurantes extends Activity {
 	  	  		Intent intent = new Intent(GeneralRestaurantes.this, InfoRestaurante.class);
 	  	  		intent.putExtra("nombre", restaurantes.get(posicion).getNombreRestaurante());
 	  	  		intent.putExtra("calle", restaurantes.get(posicion).getCalle());
+	  	  		intent.putExtra("cp", restaurantes.get(posicion).getCP());
+	  	  		intent.putExtra("poblacion", restaurantes.get(posicion).getPoblacion());
 	  	  		intent.putExtra("telefono", restaurantes.get(posicion).getTelefono());
-	  	  		intent.putExtra("logo", restaurantes.get(posicion).getImagen()); 
 	  	  		intent.putExtra("imagen",restaurantes.get(posicion).getImagenFachada());
 	  	  		intent.putExtra("id", restaurantes.get(posicion).getIdRestaurante());
 	  	  		
@@ -74,48 +79,50 @@ public class GeneralRestaurantes extends Activity {
 
 
 	public ArrayList<PadreListRestaurantes> obtenerRestaurantes() {
+
+		ArrayList<PadreListRestaurantes> restaurantes = new ArrayList<PadreListRestaurantes>();
+		restaurantes.add(new PadreListRestaurantes("Vips Princesa",3,"Calle de la Princesa 5", "28008", "Madrid", "vips", "vips_princesa5", "+34912752063"));
+		restaurantes.add(new PadreListRestaurantes("Vips Goya",4,"Calle Goya 67", "28020", "Madrid", "vips", "vips_goya67", "+34912752213"));
+		restaurantes.add(new PadreListRestaurantes("Vips Sanchinarro",5,"Avenida de Burgos 119", "28050", "Madrid", "vips", "vips_sanchinarro119", "+34915556677"));
+		restaurantes.add(new PadreListRestaurantes("Foster Princesa",6,"Calle de la Princesa 13", "28039", "Madrid", "logo_foster", "foster_princesa13", "+34915591914"));
+		restaurantes.add(new PadreListRestaurantes("Foster Ópera",7,"Plaza Isabel II 3", "28076", "Madrid", "logo_foster", "foster_opera3","+34914678900"));
 	
 		//Importamos la base de datos
-		try {
-			sqlRestaurantes = new HandlerGenerico(getApplicationContext(),
-					"/data/data/com.example.nfcook_gerente/databases/",
-					"Restaurantes.db");
-			dbRestaurantes = sqlRestaurantes.open();
-		} catch (SQLiteException e) {
-			System.out.println("CATCH");
-			Toast.makeText(getApplicationContext(),
-					"NO EXISTE LA BASE DE DATOS", Toast.LENGTH_SHORT).show(); 
-		}
-		
-		//Leemos los datos de la base de datos
-		try{
-		String[] campos = new String[]{"Nombre","Calle","Telefono","ImagenRest","ImagenFachada","id"};
-	   
-		//String[] datos = new String[]{"Vips Goya"};
-		Cursor c = dbRestaurantes.query("Restaurantes", campos, null, null, null, null,null);
-	    
-		//Cargamos los datos en la los atributos correspondientes de la clase
-		restaurantes = new ArrayList<PadreListRestaurantes>();
-		while(c.moveToNext()){
-			restaurantes.add(new PadreListRestaurantes(	c.getString(0),	//nombre
-														c.getInt(5),	//id
-														c.getString(1), //calle
-														c.getString(3), //imagen
-														c.getString(4), //imagenFachada
-														c.getString(2)));//telefono
-		}
-	   
-		}catch(Exception e){
-			System.out.println("Error en la carga de Restaurantes");
-		}
+//		try {
+//			sqlRestaurantes = new HandlerGenerico(getApplicationContext(),
+//					"/data/data/com.example.nfcook_gerente/databases/",
+//					"Restaurantes.db");
+//			dbRestaurantes = sqlRestaurantes.open();
+//		} catch (SQLiteException e) {
+//			System.out.println("CATCH");
+//			Toast.makeText(getApplicationContext(),
+//					"NO EXISTE LA BASE DE DATOS", Toast.LENGTH_SHORT).show(); 
+//		}
+//		
+//		//Leemos los datos de la base de datos
+//		try{
+//		String[] campos = new String[]{"Nombre","Calle","Telefono","ImagenRest","ImagenFachada","id"};
+//	   
+//		//String[] datos = new String[]{"Vips Goya"};
+//		Cursor c = dbRestaurantes.query("Restaurantes", campos, null, null, null, null,null);
+//	    
+//		//Cargamos los datos en la los atributos correspondientes de la clase
+//		restaurantes = new ArrayList<PadreListRestaurantes>();
+//		while(c.moveToNext()){
+//			restaurantes.add(new PadreListRestaurantes(	c.getString(0),	//nombre
+//														c.getInt(5),	//id
+//														c.getString(1), //calle
+//														c.getString(3), //imagen
+//														c.getString(4), //imagenFachada
+//														c.getString(2)));//telefono
+//		}
+//	   
+//		}catch(Exception e){
+//			System.out.println("Error en la carga de Restaurantes");
+//		}
 
 		
-//		ArrayList<PadreListRestaurantes> restaurantes = new ArrayList<PadreListRestaurantes>();
-//		restaurantes.add(new PadreListRestaurantes("Vips Princesa",3,"Calle de la Princesa 5, 28008, Madrid", "vips", "vips_princesa5", "+34912752063"));
-//		restaurantes.add(new PadreListRestaurantes("Vips Goya",4,"Calle Goya 67, Madrid", "vips", "vips_goya67", "+34912752213"));
-//		restaurantes.add(new PadreListRestaurantes("Vips Sanchinarro",5,"Avenida de Burgos 119, Las Tablas, 28050 Madrid", "vips", "vips_sanchinarro119", "+34915556677"));
-//		restaurantes.add(new PadreListRestaurantes("Foster Princesa",6,"Calle de la Princesa 13, Madrid", "logo_foster", "foster_princesa13", "+34915591914"));
-//		restaurantes.add(new PadreListRestaurantes("Foster Ópera",7,"Plaza Isabel II, 3 Madrid", "logo_foster", "foster_opera3","+34914678900"));
+
 		return restaurantes;
 	}
 	
@@ -130,7 +137,14 @@ public class GeneralRestaurantes extends Activity {
 		
     	botonComparar.setVisibility(8); 
     	botonAceptar.setVisibility(0);
+
+    	//botonAceptar.setWidth(vista.getWidth()/2);
+
     	botonCancelar.setVisibility(0);
+
+    	//botonCancelar.setWidth(vista.getWidth()/2);
+
+
     	
 		adapterListGeneralRestaurantes = new MiListGeneralRestaurantesAdapter(GeneralRestaurantes.this, restaurantes);
 		listViewRestaurantes.setAdapter(adapterListGeneralRestaurantes);
