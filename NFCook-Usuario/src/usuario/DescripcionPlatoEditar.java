317947e0695cec7ac2ee00b579d68a622d67b19c
javikase10 <javikase10@3336fd84-853c-92c1-6b51-ecfb7933bf2d>
2013-11-18 17:42:53 +0000

    Arreglos en descripcion de plato Por fin funciona los ingredientes al editar un plato

--- NFCook-Usuario/src/usuario/DescripcionPlatoEditar.java
+++ NFCook-Usuario/src/usuario/DescripcionPlatoEditar.java
@@ -9,34 +9,37 @@ import fragments.PedidoFragment;
 import baseDatos.HandlerDB;
 import adapters.HijoExpandableListEditar;
 import adapters.MiExpandableListAdapterEditar;
+import adapters.MiGridViewSeleccionarIngredientesPlato;
 import adapters.PadreExpandableListEditar;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.FrameLayout;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.FrameLayout.LayoutParams;
 
 public class DescripcionPlatoEditar extends Activity {
-		
-	private AutoCompleteTextView actwObservaciones;
     	
     private static ExpandableListView expandableListPedidoEditar;
 	private static MiExpandableListAdapterEditar adapterExpandableListPedidoEditar;
 			
-	private String observaciones, idPlato, nombrePlato;
-	private String idHijo;
+	private static ArrayList<Boolean> ingredientesMarcado;
+	private ArrayList<String> ingredientes;
+	
+	private String idPlato, nombrePlato, idHijo;
 	
 	public HandlerDB sql,sqlPedido;
 	public SQLiteDatabase db,dbPedido;
@@ -60,34 +63,27 @@ public class DescripcionPlatoEditar extends Activity {
     	
         setContentView(R.layout.descripcion_del_plato_editar);
         
-        TextView t=(TextView)findViewById(R.id.descripcionEditar);
-		LayoutParams a =new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT,t.getLineHeight()*2);//ancho,largo);
+        TextView textViewDescripcion =(TextView)findViewById(R.id.descripcionEditar);
+		LayoutParams a =new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT,textViewDescripcion.getLineHeight()*2);//ancho,largo);
 		a.setMargins(52, 0, 5, 0);
-		t.setLayoutParams(a);
-       
+		textViewDescripcion.setLayoutParams(a);
+		
         TextView textViewNombrePlato= (TextView) findViewById(R.id.nombrePlato);
-        TextView textViewDescripcion= (TextView) findViewById(R.id.descripcionEditar);
         ImageView imgeViewPlato = (ImageView) findViewById(R.id.imagenPlato);
-        Button botonConfirmar = (Button) findViewById(R.id.botonOpcion);
-        Button botonEditar = (Button) findViewById(R.id.botonOpcionEditar);
-        botonConfirmar.setVisibility(Button.INVISIBLE);
-        botonEditar.setVisibility(Button.VISIBLE);
-
-        actwObservaciones = (AutoCompleteTextView)findViewById(R.id.AutoCompleteTextViewOpciones);
-      
+        
 
         // Obtenemos el nombre del plato y el restaurante de la pantalla anterior
         Bundle bundle = getIntent().getExtras();
         idPlato = bundle.getString("idPlato");
         String extras = bundle.getString("extras");
-        observaciones = bundle.getString("observaciones");
+        String ingredientesStr = bundle.getString("ingredientes");
         idHijo = bundle.getString("idHijo");
      
         // Importamos la base de datos para su posterior lectura
         importarBaseDatatos();
 
         // Hacemos una consulta en la base de datos sobre el plato seleccionado   
-        String[] campos =new String[]{"Extras","Precio","Foto","Descripcion","Nombre"};
+        String[] campos =new String[]{"Extras","Precio","Foto","Descripcion","Nombre","Ingredientes"};
         String[] datos = new String[]{idPlato};
         Cursor cursor =db.query("Restaurantes",campos,"Id =?",datos,null,null,null);  
       
@@ -98,14 +94,14 @@ public class DescripcionPlatoEditar extends Activity {
         String descripcion = cursor.getString(3);
         nombrePlato = cursor.getString(4);
         String extrasBusqueda = cursor.getString(0);
+        String ingredientesBusqueda = cursor.getString(5);
     	
         if (!extrasBusqueda.equals("")){
             String[] tokens = extrasBusqueda.split("/");
             String[] extrasSeleccionados = extras.split(",");
 	        // Creamos los padres de la lista, serán las distintas categorías de extras
 	        ArrayList<PadreExpandableListEditar> categoriasExtras =  new ArrayList<PadreExpandableListEditar>();
-	        for(int i= 0; i< tokens.length ;i++)
-	        {	
+	        for(int i= 0; i< tokens.length ;i++){	
 	        	String[] nombreExtra = null;
 	        	String extraSeleccionadoPradreI = extrasSeleccionados[i];
 				try{
@@ -158,31 +154,114 @@ public class DescripcionPlatoEditar extends Activity {
         // Damos el texto a los textviews
     	textViewNombrePlato.setText(nombrePlato);
     	textViewDescripcion.setText(descripcion);
+    	
+    	ingredientes = new ArrayList<String>();
+        ingredientesMarcado = new ArrayList<Boolean>();
+        String[] ingredientesSeleccionados = ingredientesStr.split(",");
+        
+    	if (!ingredientesBusqueda.equals("")){
+    		
+    		if (ingredientesStr.equals("Con todos los ingredientes")){
+    			String[] tokens = ingredientesBusqueda.split("%");
+        		for (int i=0; i<tokens.length; i++){
+        			ingredientes.add(tokens[i]);
+        			ingredientesMarcado.add(true);
+        		}
+    		} else {
+    			String[] tokens = ingredientesBusqueda.split("%");
+        		for (int i=0; i<tokens.length; i++){
+        			ingredientes.add(tokens[i]);
+        			
+        			int j = 0, espacio = 0; // espacios es para comparar sin espacios
+        			boolean marcado = false;
+        			while(!marcado && j <ingredientesSeleccionados.length){        		
+        				if (tokens[i].toLowerCase().equals(ingredientesSeleccionados[j].substring(4 + espacio)))
+        					marcado = true;
+        				else 
+        					j++;
+        				if (j == 1) espacio = 1;
+        			}
+        			if (marcado)
+        				ingredientesMarcado.add(false);
+        			else 
+        				ingredientesMarcado.add(true);
+        		}
+    		}
+    		
+    	}
+        
+        
+        ImageView botonIngredientes = (ImageView) findViewById(R.id.botonIngredientes);
+        botonIngredientes.setOnClickListener(new View.OnClickListener() {
+			
+
+			public void onClick(View v) {
+				
+				if(ingredientes.size() == 0){
+		    		Toast.makeText(getApplicationContext(), "Ingredientes no disponibles", Toast.LENGTH_SHORT).show();
+				}else{
+			
+					/*************PREPARAMOS EL LAYOUT DE LA VENTANA EMERGENTE**************/
+					AlertDialog.Builder ventanaEmergente = new AlertDialog.Builder(DescripcionPlatoEditar.this);           
+					// Creamos la vista que tendrá la ventana de elección de ingredientes
+					View vistaVentanaEmergente = LayoutInflater.from(DescripcionPlatoEditar.this).inflate(R.layout.ventana_emergente_elegir_ingredientes, null);
+					// Añadimos el botón Aceptar
+					ventanaEmergente.setNegativeButton("Aceptar", null);
+					
+					GridView gridViewEleccionIngredientes = (GridView) vistaVentanaEmergente.findViewById(R.id.gridViewElegirIngredientes);
+					MiGridViewSeleccionarIngredientesPlato miGridViewSeleccionarIngredientesPlato = new MiGridViewSeleccionarIngredientesPlato(DescripcionPlatoEditar.this, ingredientes, ingredientesMarcado );
+					gridViewEleccionIngredientes.setAdapter(miGridViewSeleccionarIngredientesPlato);
+										
+					// Aplicamos la vista a la ventana y la lanzamos
+					ventanaEmergente.setView(vistaVentanaEmergente);
+					ventanaEmergente.show();
+					/********************FIN VENTANA EMERGENTE REPARTO*********************/
+				}
+			}
+		});
+    	
 	}
 	
 	public void onClickConfirmarEditar(View v){
 		importarBaseDatatosPedido();
 		String nuevosExtrasMarcados = null;
 		String nuevosExtrasBinarios = null;
+		String ingredientesBinarios = null;
+		String ingredientesStr = null;
+		
+		
 		if(adapterExpandableListPedidoEditar != null){ // El plato tiene extras
 			nuevosExtrasMarcados = adapterExpandableListPedidoEditar.getExtrasMarcados();
 			nuevosExtrasBinarios = adapterExpandableListPedidoEditar.getExtrasBinarios();
 		}
+	
+		if(ingredientesMarcado.size() > 0){
+			ingredientesStr = "";
+			ingredientesBinarios = "";
+			for(int i=0; i<ingredientesMarcado.size(); i++){
+				if(ingredientesMarcado.get(i)) // == true
+					ingredientesBinarios += "1";
+				else{						   // == false
+					ingredientesBinarios += "0";
+					ingredientesStr += ingredientes.get(i) + ", sin ";
+				}
+			}
+			if (ingredientesStr.equals("")){
+				ingredientesStr = "Con todos los ingredientes";
+			} else {
+				ingredientesStr = "Sin " + ingredientesStr.substring(0, ingredientesStr.length()-6).toLowerCase();
+			}    			
+		}
 		
-    	String observacionesNuevas;
-    	if(!actwObservaciones.getText().toString().equals("")){
-    		observacionesNuevas = actwObservaciones.getText().toString();
-    	}else{
-    		observacionesNuevas = this.observaciones;
-    	}
     	ContentValues platoEditado = new ContentValues();
     	platoEditado.put("Id", idPlato);
     	platoEditado.put("Plato", nombrePlato);
     	platoEditado.put("Extras", nuevosExtrasMarcados);
     	platoEditado.put("ExtrasBinarios", nuevosExtrasBinarios);    	
-    	platoEditado.put("Observaciones", observacionesNuevas);
-        String[] camposUpdate = {idPlato,idHijo};
-        dbPedido.update("Pedido", platoEditado, "Id =? AND IdHijo=?", camposUpdate);
+    	platoEditado.put("Ingredientes", ingredientesStr);
+    	platoEditado.put("IngredientesBinarios", ingredientesBinarios);
+    	String[] camposUpdate = {idPlato,idHijo};
+    	dbPedido.update("Pedido", platoEditado, "Id =? AND IdHijo=?", camposUpdate);
         PedidoFragment.actualizaExpandableListPedidoEditada();
         this.finish();
 	}
@@ -222,13 +301,10 @@ public class DescripcionPlatoEditar extends Activity {
 		
 	}
     
-    public void onclickLayout(View v)
-    {
+    public void onclickLayout(View v) {
 		
 		TextView t=(TextView)findViewById(R.id.descripcionEditar);
 		ImageView image=(ImageView) findViewById(R.id.imageViewEditar);
-		
-    	
 		
 		if (!pulsado){
 			pulsado=true;
