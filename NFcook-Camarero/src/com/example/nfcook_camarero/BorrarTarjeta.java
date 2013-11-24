e80fbdc09b397573191277ade3f0ded39a5a8732
javikase10 <javikase10@3336fd84-853c-92c1-6b51-ecfb7933bf2d>
2013-11-24 09:04:32 +0000

    borrado de tarjeta completo

--- NFcook-Camarero/src/com/example/nfcook_camarero/BorrarTarjeta.java
+++ NFcook-Camarero/src/com/example/nfcook_camarero/BorrarTarjeta.java
@@ -24,7 +24,6 @@ import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.TagLostException;
-import android.nfc.tech.MifareClassic;
 import android.nfc.tech.Ndef;
 import android.nfc.tech.NdefFormatable;
 import android.os.AsyncTask;
@@ -38,7 +37,6 @@ import android.widget.Toast;
 
 public class BorrarTarjeta extends Activity implements DialogInterface.OnDismissListener{
 
-	//Variables usadas para el nfc
 	NfcAdapter adapter;
 	PendingIntent pendingIntent;
 	IntentFilter writeTagFilters[];
@@ -46,12 +44,11 @@ public class BorrarTarjeta extends Activity implements DialogInterface.OnDismiss
 	Context ctx;
 	String[][] mTechLists;
 	boolean escritoBienEnTag;
+	String restaurante, codigoRest, abreviaturaRest;
 	ProgressDialog	progressDialogSinc;
-	String restaurante;
-	int numeroRestaurante;
 
 	private SQLiteDatabase dbEquivalencia;
-	private HandlerGenerico sql;
+	private HandlerGenerico sqlEquivalencia;
 	
 	//Variables para el sonido
 	SonidoManager sonidoManager;
@@ -82,21 +79,19 @@ public class BorrarTarjeta extends Activity implements DialogInterface.OnDismiss
 		  SystemClock.sleep(1000);
 		  // si es Mifare Classic
 			try {   
-				//read(mytag);//Se ha detectado la tag procedemos a leerla
-				
-				ArrayList<Byte> al = new ArrayList<Byte>();
-				al.add((byte)0); // FIXME cambiar toda esta mierda
-				al.add((byte)255);
-				escribirEnTagNFC(al);
+				ArrayList<Byte> tagInicial = new ArrayList<Byte>();
+				tagInicial.add((byte) Integer.parseInt(codigoRest));
+				tagInicial.add((byte)255);
+				escribirEnTagNFC(tagInicial);
 				
 				//Sonido de confirmacion
 				sonidoManager.play(sonido);
 				}
 			 catch (IOException e) {//Error en la lectura has alejado el dispositivo de la tag
-				Toast.makeText(ctx, ctx.getString(R.string.error_reading), Toast.LENGTH_LONG ).show();
+				escritoBienEnTag = false;
 				e.printStackTrace();
 			} catch (FormatException e) {
-				Toast.makeText(ctx, ctx.getString(R.string.error_reading) , Toast.LENGTH_LONG ).show();
+				escritoBienEnTag = false;
 				e.printStackTrace();
 			}
 		
@@ -146,30 +141,20 @@ public class BorrarTarjeta extends Activity implements DialogInterface.OnDismiss
         //Obtengo el resturante
   		Bundle bundle = getIntent().getExtras();
   		restaurante = bundle.getString("Restaurante");
-  		//restaurante="Foster";
-  		
-  		 try{ //Abrimos la base de datos para consultarla
- 	       	sql = new HandlerGenerico(getApplicationContext(),"Equivalencia_Restaurantes.db"); 
- 	        dbEquivalencia = sql.open();
- 	     
- 	    }catch(SQLiteException e){
- 	        	Toast.makeText(getApplicationContext(),"No existe la base de datos equivalencia",Toast.LENGTH_SHORT).show();
- 	       }
  	   
- 	   try{
- 		  /**Campos de la base de datos Restaurante TEXT,Numero INTEGER,Abreviatura TEXT
- 	        * Nombre de la tabla de esa base de datos Restaurantes*/		
- 		   String[] campos = new String[]{"Numero","Abreviatura"};
- 		   String[] datos = new String[]{restaurante};
- 		   //Buscamos en la base de datos el nombre de usuario y la contraseña
- 		   Cursor c = dbEquivalencia.query("Restaurantes",campos,"Restaurante=?",datos, null,null, null);
- 	  	   		   	 	   
- 	  	   c.moveToFirst();
-        	
- 	  	   numeroRestaurante = c.getInt(0);
- 	  	   String abreviatura = c.getString(1);
- 		}catch(Exception e){ }
+  		//obtenemos el codigo y la abreviatura del rest
+		try {
+			sqlEquivalencia = new HandlerGenerico(getApplicationContext(), "Equivalencia_Restaurantes.db");
+			dbEquivalencia = sqlEquivalencia.open();
+		} catch (SQLiteException e) {
+			System.out.println("NO EXISTE BASE DE DATOS PEDIDO: SINCRONIZAR QR");
+		}
+		obtenerCodigoYAbreviaturaRestaurante();
+		dbEquivalencia.close();
         
+		//inicializamos variables para mostrar errores
+		escritoBienEnTag = false;
+		
         // creamos el progresDialog que se mostrara
   		crearProgressDialogSinc(); 
 
@@ -186,101 +171,19 @@ public class BorrarTarjeta extends Activity implements DialogInterface.OnDismiss
   		progressDialogSinc.setCancelable(false);
   	    // listener para que ejecute el codigo de onDismiss cuando el dialog se cierre
   		progressDialogSinc.setOnDismissListener(this);
-	}
-	
+	}	
 	
 	/**
 	 * Cierra la actividad y muestra un mensaje en funcion de que haya sucedido. Se ejecuta cuando se cierra el progressDialog.
 	 * 
 	 */
 	public void onDismiss(DialogInterface dialog) {
-//		if (escritoBienEnTag) {
-//			Toast.makeText(this, "Borrado correctamente.", Toast.LENGTH_LONG ).show();		
-//		}
-//		else {
-//			Toast.makeText(this, "Borrado no completado", Toast.LENGTH_LONG ).show();		 
-//		}
+		if (escritoBienEnTag)
+			Toast.makeText(this, "Tarjeta borrada correctamente", Toast.LENGTH_LONG ).show();		
+		else
+			Toast.makeText(this, "Error al borrar la tarjeta", Toast.LENGTH_LONG ).show();		 
+		
 		finish();	
-	}
-
-	
-	//@SuppressLint("NewApi")
-	/**
-	 * Metodo que se encarga de leer bloques de la tarjeta nfc
-	 * @param tag
-	 * @throws IOException
-	 * @throws FormatException
-	 */
-	private void read(Tag tag) throws IOException, FormatException {
-		
-		
-		//TODO LUEGO HACER LECTURAS
-		
-//		String aux = "";
-//		aux += "255";
-//		ArrayList<Byte> pedidoCodificadoEnBytes = new ArrayList<Byte>();
-//		
-//		ArrayList<Byte> al = new ArrayList<Byte>();
-//		al.add((byte) numeroRestaurante);
-//		pedidoCodificadoEnBytes.addAll(al);
-//		
-//		al = new ArrayList<Byte>();
-//		al.add((byte) Integer.parseInt(aux));
-//		pedidoCodificadoEnBytes.addAll(al);
-//		
-//		
-//		// Obtenemos instancia de MifareClassic para el tag.
-//				MifareClassic mfc = MifareClassic.get(mytag);
-//												
-//				// Habilitamos operaciones de I/O
-//				mfc.connect();
-//
-//				boolean sectorValido = false;				
-//				
-//				// para recorrer string de MifareClassic.BLOCK_SIZE en MifareClassic.BLOCK_SIZE
-//				int recorrerString = 0;	
-//				// para saber si se ha escrito o no
-//				escritoBienEnTag = false;
-//				
-//				// relleno con 0's el pedido hasta que sea modulo16 para que luego no haya problemas ya que 
-//				// se escribe mandando bloques de 16 bytes
-//				int  numMod16 = pedidoCodificadoEnBytes.size() % 16;
-//				if (numMod16 != 0){
-//					int huecos = 16-numMod16;
-//					for (int i = 0; i < huecos; i++)
-//						pedidoCodificadoEnBytes.add((byte) 0);
-//				}
-//				for (int i=0; i<mfc.getBlockCount();i++)
-//				{
-//					if (sePuedeEscribirEnBloque(i)) {
-//						// cada sector tiene 4 bloques
-//						int numSector = i / 4;
-//						// autentifico con la key A para escritura
-//						sectorValido = mfc.authenticateSectorWithKeyA(numSector,MifareClassic.KEY_DEFAULT);
-//						if (sectorValido) {
-//							// si es menor significa que queda por escribir cosas
-//							if (recorrerString < pedidoCodificadoEnBytes.size()) { //textoBytes.length 
-//								// recorremos con un for para obtener bloques de 16 bytes
-//								byte[] datosAlBloque = new byte[MifareClassic.BLOCK_SIZE];
-//								for (int j=0; j<MifareClassic.BLOCK_SIZE; j++)
-//									datosAlBloque[j] = pedidoCodificadoEnBytes.get(j+recorrerString);
-//								// avanzo para el siguiente bloque
-//								recorrerString += MifareClassic.BLOCK_SIZE;
-//								// escribimos en el bloque
-//								mfc.writeBlock(i, datosAlBloque);
-//							} else {
-//								// escribimos ceros en el resto de la tarjeta porque ya no queda nada por escribir
-//								byte[] ceros = new byte[MifareClassic.BLOCK_SIZE];
-//								mfc.writeBlock(i, ceros);
-//							}
-//						}
-//					} 
-//				}
-//				escritoBienEnTag = true;
-//								
-//				// Cerramos la conexion
-//				mfc.close();
-		
 	}
 	
 	
@@ -379,12 +282,17 @@ public class BorrarTarjeta extends Activity implements DialogInterface.OnDismiss
 
 
 	/**
-	 * Metedo encargado de comprobar si se puede o no escribir en un bloque pasado por parametro
-	 * @param numBloque
-	 * @return
+	 * MEtodo que da valor al codigo y a la abreviatura del restaurante
 	 */
-	private boolean sePuedeEscribirEnBloque(int numBloque) {
-		return (numBloque+1) % 4 != 0 && numBloque != 0 ; 
+	private void obtenerCodigoYAbreviaturaRestaurante() {
+		// Campos que quieres recuperar
+		String[] campos = new String[] { "Numero", "Abreviatura" };
+		String[] datos = new String[] { restaurante };
+		Cursor cursorPedido = dbEquivalencia.query("Restaurantes", campos, "Restaurante=?",datos, null, null, null);
+		
+		cursorPedido.moveToFirst();
+		codigoRest = cursorPedido.getString(0);
+		abreviaturaRest = cursorPedido.getString(1);
 	}
 
 	/**Con este método detectamos la presencia de la tarjeta tag y establecemos la conexión
