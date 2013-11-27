e22c31f5374c5ea97fda774c8b845b9e8b50c777
abelino_091 <abelino_091@hotmail.com>
2013-11-27 17:40:40 +0000

    Arreglado bug empleado y mejorado aspecto lista empleados. Se va a proceder a un cambio en el diseño de la misma.

--- NFCook-Gerente/src/adapters/PadreListaEmpleados.java
+++ NFCook-Gerente/src/adapters/PadreListaEmpleados.java
@@ -19,10 +19,6 @@ public class PadreListaEmpleados {
 	private String puesto;
 	private String idEmpleado;
 	
-	
-	
-	
-	
 	public PadreListaEmpleados(String foto,String nombre, String apellido1 , String apellido2, String puesto,String idEmpleado){
 		this.foto = foto;
 		this.nombre = nombre;
@@ -30,59 +26,47 @@ public class PadreListaEmpleados {
 		this.apellido2 = apellido2;
 		this.puesto = puesto;
 		this.idEmpleado = idEmpleado;
-		
 	}
-
 
 	public String getFoto() {
 		return foto;
 	}
 
-
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 
 	public String getApellido1() {
 		return apellido1;
 	}
 
-
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
 	}
-
 
 	public String getApellido2() {
 		return apellido2;
 	}
 
-
 	public void setApellido2(String apellido2) {
 		this.apellido2 = apellido2;
 	}
-
 
 	public String getDni() {
 		return dni;
 	}
 
-
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
-
 
 	public String getPuesto() {
 		return puesto;
@@ -91,7 +75,6 @@ public class PadreListaEmpleados {
 	public void setPuesto(String puesto){
 		this.puesto = puesto;
 	}
-
 
 	public String getIdEmpleado() {
 		return idEmpleado;
